66f807c5a51ba15d246e98dfbed55fe708e53d93
ivan wolyniec <ivanwolyniec@gmail.com>
2022-07-12 22:57:33 -0400

    added default route, region specific dream GET, comments, and todo guidance

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-// import jwtDecode from "jwt-decode";
+//  ! ! ! ! ! ! !
+//  !  IMPORTS  !
+//  ! ! ! ! ! ! !
 const {
   TableClient,
   TableServiceClient,
@@ -7,26 +9,15 @@ const {
 const jwtDecode = require("jwt-decode");
 const express = require("express");
 const cors = require("cors");
+//  ! ! ! ! ! ! !
+//  !   SETUP   !
+//  ! ! ! ! ! ! !
 const app = express();
-app.use(cors());
-app.use(express.json());
 app.use(express.urlencoded());
-// let date = Date.now()
-// console.log(date);
-// console.log(Date.parse(1657588049));
-//
-// testing api shit
-//
-// let testToken =
-//   "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ilg1ZVhrNHh5b2pORnVtMWtsMll0djhkbE5QNC1jNTdkTzZRR1RWQndhTmsifQ.eyJleHAiOjE2NTc1ODgwNDksIm5iZiI6MTY1NzU4NDQ0OSwidmVyIjoiMS4wIiwiaXNzIjoiaHR0cHM6Ly9kcjNhbXNwYWNlLmIyY2xvZ2luLmNvbS8yNzUwY2JjOC05NTRlLTRjYTUtYjQ4ZS1jZTBkYjRiZDVlZWEvdjIuMC8iLCJzdWIiOiIwZmQ0NWQzYi02ZGIzLTRmNGMtYjY0NS0xZmMzNDVjOTE4ZjciLCJhdWQiOiIwNWNlMmVmOC1jZjYyLTQ0NDItYTE3OC1kOGNlZjQ3NDA1YjAiLCJub25jZSI6IjZjOGQ3ZGMwLTE5MDAtNDRjOC1hZjkxLTU0NjQ2OWEwZjg3YSIsImlhdCI6MTY1NzU4NDQ0OSwiYXV0aF90aW1lIjoxNjU3NTg0NDQ2LCJvaWQiOiIwZmQ0NWQzYi02ZGIzLTRmNGMtYjY0NS0xZmMzNDVjOTE4ZjciLCJnaXZlbl9uYW1lIjoiaXZhbiIsInRmcCI6IkIyQ18xX2VtYWlsX2FuZF9nb29nbGUifQ.blexRadRHxsOc8IK0xdikQr-2sxxAgd1pq2TEwMVS0PzzPjslfEqHSryl_MiYFo6FPHe8fPrShmm3uEK_SYnc9cju7ou2DYJS942xPqqiV83B-8uBkdgim9qz9jD71RXJZ34KCv9pV5aIQ-1X3CWOQ5QIuLMRx5u7sTS1JLKysbd2t_GvWzG32FtN2UiOrb7J8at3nOhREXsvJThjW7C8lOtPkv2WEPs-WVEu6kpMhWEb4440bvbFGwYDX6wjRBErPWgbWrKfVsqr9yFUv2gD_M5Hwl9Tdwlm8du8GwMGdzJAyai4k18ZhojlHKxpN1haVhklLEcfGvzhpXmnGqJAw";
-// let decodedTestToken = jwtDecode(testToken);
-// console.log(decodedTestToken);
+app.use(express.json());
+app.use(cors());
 
-// if(1657588049 < new Date()/1000) console.log('expired');
-// else console.log('not expired');
-
-// DOCS LMAO
-//
+// TODO: parameterize these into environment variables
 const tableCredential = new AzureNamedKeyCredential(
   "sadr3amspace",
   "LgATTCXQjEGD6bd7jkmHrbGVMvpYqmpSLakNeW07SnqWpqVrRTCesjRaUhTR8/IUFt2mO89DenN4+ASt/nBlow=="
@@ -41,9 +32,10 @@ const tableServiceClient = new TableServiceClient(tableUrl, tableCredential);
 // some of that is rather unhelpfully explained in the following docs:
 // https://docs.microsoft.com/en-us/azure/cosmos-db/table/how-to-use-nodejs
 
-// tableServiceClient.createTable('mytable');
-// utility functions
-// TODO: spin these off into separate files when they work
+//  ! ! ! ! ! ! ! !
+//  !  UTILITIES  !
+//  ! ! ! ! ! ! ! !
+// TODO: spin these off into separate files when they actually work
 let validateAuthToken = (authToken) => {
   // decode token. if invalid, error
   // check expiration date. if expired, error
@@ -60,16 +52,16 @@ let validateDreamContent = (_) => {
   // :P
 };
 
+//  ! ! ! ! ! ! !
+//  !  ROUTES   !
+//  ! ! ! ! ! ! !
+// default endpoint, functions as health check
 app.get("/", function (req, res) {
   res.json({ status: "api active" });
 });
 
-// the dream creation endpoint
+// dream creation
 app.post("/dream", cors(), async function (req, res) {
-  console.log("request");
-  console.log(req);
-  console.log("auth token");
-  console.log(req.headers.authorization);
   // validate the auth token
   if (!validateAuthToken(req.headers.authorization)) {
     res.statusCode = 401;
@@ -80,30 +72,107 @@ app.post("/dream", cors(), async function (req, res) {
   console.log(req.body);
   if (!validateDreamContent(newDream)) {
     res.statusCode = 400;
-    res.json({ status: "malformed dream body" });
+    res.json({ status: "malformed dream" });
   }
   // add dream to storage table
   var date = Date.now();
-  var key = date + "-" + Math.ceil(Math.random() * 1000);
+  var key = date + "-" + Math.ceil(Math.random() * 1000); // TODO: get better guiding system, this is TRASH lol
   newDream["partitionKey"] = "dreams";
   newDream["rowKey"] = key;
   let dreamCreateResult = await tableClient.createEntity(newDream);
-  console.log("result of dream addition process:");
-  console.log(dreamCreateResult);
-  // TODO:  determine if this actually works lmao
+  // TODO:  return 409 if the data already exists in the SA
   res.statusCode = 201;
   res.json({ status: "created", dreamId: key });
 });
 
-app.get("/i", function (req, res) {
-  console.log(req.headers);
-  let token = req.headers.authorization;
-  let decodedToken = jwtDecode(token);
+// get a specific dream based on dream partition key
+app.get("/dream/:id", cors(), async function (req, res) {
+    //validate the auth token
+    if (!validateAuthToken(req.headers.authorization)) {
+      res.statusCode = 401;
+      res.json({ status: "invalid token" });
+    }
+    // get dream id from url
+    let dreamId = req.params.id;
+    try {
+      // get dream from storage account
+      let dreamFetchResult = await tableClient.getEntity("dreams", dreamId);
+      res.statusCode = 200;
+      res.json(dreamFetchResult);
+    } catch {
+      res.statusCode = 500;
+      res.json({ status: "problem getting dream" });
+    }
+  });
+
+// get all dreams
+app.get("/dreams", cors(), async function (req, res) {
+  //validate the auth token
+  if (!validateAuthToken(req.headers.authorization)) {
+    res.statusCode = 401;
+    res.json({ status: "invalid token" });
+  }
+  let entities = tableClient.listEntities();
+  //   this is an EXPENSIVE routine, we have to refine this and cache AS MUCH DATA AS POSSIBLE server side
+  // though now that i consider, i wonder what hosting charges that might incur?
+  // TODO: explore cheap deployment platform for workhorse api
+  try {
+    let dreamList = [];
+    for await (const entity of entities) {
+      dreamList.push(entity);
+    }
+    res.statusCode = 200;
+    res.json({ dreams: dreamList });
+  } catch {
+    res.statusCode = 500;
+    res.json({ status: "problem" });
+  }
+});
+
+// get geographical region specific dreams
+// TODO: come up with a querying scheme that cuts down on the unnecessary processing 
+app.get("/dreams/geo", cors(), async function (req, res) {
+  //validate the auth token
+  if (!validateAuthToken(req.headers.authorization)) {
+    res.statusCode = 401;
+    res.json({ status: "invalid token" });
+  }
+  // okay so this one is weirder, to get table entities that match a specific query we have to tunnel into the options a little bit and use
+  // the fucking "OData filter expressions i hate so much"
+  // docs: https://www.odata.org/getting-started/basic-tutorial/
+  let entities = tableClient.listEntities({
+    queryOptions: { filter: "location eq 'detroit' " },
+  });
+  try {
+    let dreamList = [];
+    for await (const entity of entities) {
+      dreamList.push(entity);
+    }
+    res.statusCode = 200;
+    res.json({ dreams: dreamList });
+  } catch {
+    res.statusCode = 500;
+    res.json({ status: "problem" });
+  }
+});
+
+// test endpoint to validate jwt token
+app.get("/jwt", function (req, res) {
+  // check if token is valid
+  let decodedToken;
+  try {
+    let token = req.headers.authorization;
+    decodedToken = jwtDecode(token);
+  } catch {
+    res.statusCode = 400;
+    res.json({ status: "invalid token" });
+    return;
+  }
   if (
     decodedToken.iss ==
       `https://dr3amspace.b2clogin.com/2750cbc8-954e-4ca5-b48e-ce0db4bd5eea/v2.0/` &&
     decodedToken.aud == `05ce2ef8-cf62-4442-a178-d8cef47405b0` &&
-    decodedToken.exp < new Date() / 1000
+    decodedToken.exp < new Date() / 1000 // TODO: figure out what's screwy with the date comparison here. Unix UTC timecode vs node default?
   ) {
     res.json({ status: "token checks out, authorized api call" });
   } else {
@@ -111,5 +180,14 @@ app.get("/i", function (req, res) {
   }
 });
 
+// FALLBACK ROUTE
+app.get("*", function (req, res) {
+    res.statusCode = 404;
+    res.json({status: "route not found"});
+})
+
 console.log("api started up: listening on port 3000");
 app.listen(3000);
+// TODO: set up any logging at all for insights into what's going on once we hit the server side
+// TODO: set up caching on GET routes to ensure we're not slamming the storage account with requests for data we got 0.25 seconds ago
+// https://medium.com/the-node-js-collection/simple-server-side-cache-for-express-js-with-node-js-45ff296ca0f0
